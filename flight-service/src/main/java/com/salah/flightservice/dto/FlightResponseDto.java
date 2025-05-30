0e39b55fe1b60ade6e46f3a278b21996ae435b9a
Maxime9910 <chengwenxin10@gmail.com>
2025-05-30 09:30:08 +0200

    ajoute le flightnumber dans search

--- flight-service/src/main/java/com/salah/flightservice/dto/FlightResponseDto.java
+++ flight-service/src/main/java/com/salah/flightservice/dto/FlightResponseDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class FlightResponseDto {
     private Long flightId;
+    private String flightNumber;
     private String departureCity;
     private String arrivalCity;
     private LocalDate departureDate;
