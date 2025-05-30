135c5d22dc625dec5799e84a228a1267e45fc630
Maxime9910 <chengwenxin10@gmail.com>
2025-05-30 08:54:22 +0200

    Implémenter le microservice flight pour aircraftclient

--- flight-service/src/main/java/com/salah/flightservice/controller/FlightController.java
+++ flight-service/src/main/java/com/salah/flightservice/controller/FlightController.java
@@ -5,10 +5,7 @@ import com.salah.flightservice.model.Flight;
 import com.salah.flightservice.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -32,12 +29,12 @@ public class FlightController {
     }
 
     @GetMapping("/{id}")
-    public Flight getFlightById(@RequestParam Long id) {
+    public Flight getFlightById(@PathVariable Long id) {
         return flightService.getFlights().stream().filter(flight -> flight.getFlightId().equals(id)).findFirst().orElse(null);
     }
 
     @GetMapping("/{id}/price")
-    public Double getPriceByFlightId(@RequestParam Long id) {
+    public Double getPriceByFlightId(@PathVariable Long id) {
         return flightService.getFlights().stream().filter(flight -> flight.getFlightId().equals(id)).findFirst().map(Flight::getPrice).orElse(null);
     }
 //
