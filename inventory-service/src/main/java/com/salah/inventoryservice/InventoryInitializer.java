aad78781d31267c33641fa0a695858953d7fea14
Salaheddine Benkhanous <benkhanoussalah@gmail.com>
2025-07-03 23:17:42 +0200

    feat: adding auto seats for flights

--- inventory-service/src/main/java/com/salah/inventoryservice/InventoryInitializer.java
+++ inventory-service/src/main/java/com/salah/inventoryservice/InventoryInitializer.java
@@ -1,4 +1,38 @@
 package com.salah.inventoryservice;
 
-public class InventoryInitializer {
+
+import com.salah.inventoryservice.client.FlightClient;
+import com.salah.inventoryservice.dto.CreateInventoryRequestDto;
+import com.salah.inventoryservice.dto.FlightDto;
+import com.salah.inventoryservice.service.InventoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class InventoryInitializer implements CommandLineRunner {
+
+    private final InventoryService inventoryService;
+    private final FlightClient flightClient;
+
+    public InventoryInitializer(InventoryService inventoryService, FlightClient flightClient) {
+        this.inventoryService = inventoryService;
+        this.flightClient = flightClient;
+    }
+
+    @Override
+    public void run(String... args) {
+        try {
+            List<FlightDto> flights = flightClient.getFlights(); // méthode à ajouter dans FlightClient
+
+            for (FlightDto flight : flights) {
+               CreateInventoryRequestDto createInventoryRequestDto= new CreateInventoryRequestDto(flight.getFlightId(), 200);
+                inventoryService.createInventory(createInventoryRequestDto);
+                System.out.println("✅ Inventaire créé pour le vol ID: " + flight.getFlightId());
+            }
+        } catch (Exception e) {
+            System.err.println("❌ Erreur pendant l'initialisation des inventaires: " + e.getMessage());
+        }
+    }
 }
--- inventory-service/src/main/java/com/salah/inventoryservice/client/FlightClient.java
+++ inventory-service/src/main/java/com/salah/inventoryservice/client/FlightClient.java
@@ -5,9 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "flight-service",url = "http://localhost:8081/flights")
 public interface FlightClient {
     @GetMapping("/{id}")
     FlightDto getFlightById(@PathVariable("id") Long flightId);
+
+    @GetMapping()
+    List<FlightDto> getFlights();
 }
 
